50258d6fb97f1fe32524e85b218bd3d4627a8b0d
Farshid Rezaei <farshidrezaeih@gmail.com>
2025-10-18 10:06:02 +0330

    Restrict set access for some props in BirSignAuthenticationOptions

--- src/NetFrameworkExtension/Options/BirSignAuthenticationOptions.cs
+++ src/NetFrameworkExtension/Options/BirSignAuthenticationOptions.cs
@@ -41,15 +41,15 @@ namespace MapIdeaHub.BirSign.NetFrameworkExtension.Options
             };
         }
 
-        public string ClientId { get; set; }
+        public string ClientId { get; private set; }
 
-        public string ClientSecret { get; set; }
+        public string ClientSecret { get; private set; }
 
-        public string Authority { get; set; }
+        public string Authority { get; private set; }
 
-        public string RedirectUri { get; set; }
+        public string RedirectUri { get; private set; }
 
-        public string PostLogoutRedirectUri { get; set; }
+        public string PostLogoutRedirectUri { get; private set; }
 
         public string AuthenticationType { get; set; }
 
