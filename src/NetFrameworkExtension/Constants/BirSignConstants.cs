969b4267caf7bae1b1997fe05d87d46ada829572
Farshid Rezaei <farshidrezaeih@gmail.com>
2025-10-22 09:04:31 +0330

    Add IsUseBirSign app setting

--- src/NetFrameworkExtension/Constants/BirSignConstants.cs
+++ src/NetFrameworkExtension/Constants/BirSignConstants.cs
@@ -1,4 +1,7 @@
-﻿namespace MapIdeaHub.BirSign.NetFrameworkExtension.Constants
+﻿using System;
+using System.Configuration;
+
+namespace MapIdeaHub.BirSign.NetFrameworkExtension.Constants
 {
     public class BirSignConstants
     {
@@ -6,5 +9,9 @@
         public const string ResponseType = "id_token";
         public const string Scope = "openid profile";
         public const string LoginUri = "/BirSign/Login";
+
+        public static bool IsUseBirSign =
+            ConfigurationManager.AppSettings["IsUseBirSign"] != null &&
+            Convert.ToBoolean(ConfigurationManager.AppSettings["IsUseBirSign"]);
     }
 }
