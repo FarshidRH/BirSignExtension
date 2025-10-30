7ad65eeee5891561a223b4d5065d19730e21013f
Farshid Rezaei <farshidrezaeih@gmail.com>
2025-10-30 10:31:38 +0330

    Release NetFrameworkExtension v1.0.5.0

--- src/NetFrameworkExtension/scripts/nuget/_04-nuget-push.bat
+++ src/NetFrameworkExtension/scripts/nuget/_04-nuget-push.bat
@@ -4,7 +4,7 @@
 cd /d "%~dp0"
 cd ../..
 
-set package=MapIdeaHub.BirSign.NetFrameworkExtension.1.0.4.nupkg
+set package=MapIdeaHub.BirSign.NetFrameworkExtension.1.0.5.nupkg
 set apiKey=oy2jijluovipjvh6iwravtvs4xmj7jje7esqqxnyedwsqm
 set source=https://api.nuget.org/v3/index.json
 
